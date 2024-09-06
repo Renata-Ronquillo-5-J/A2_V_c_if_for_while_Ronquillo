b30412dda1190c858e0b141909bbd85d84b74f97
Renata-Ronquillo <a.22308051281307@cbtis128.edu.mx>
2024-09-06 09:57:19 -0600

    Add files via upload
    Ejemplo if

---
+++ ejemploif.py
@@ -0,0 +1,7 @@
+
+
+edad=16 #Primero se evalua #Primera linea se le asigna un valor, en este caso 17     Variable 
+if edad >= 18: ## compara la variable con el valor asignado                  Primero evalua la seleccion     Si
+    print("Puedes entrar a bailar ♥")
+else: ##Si no
+    print("Vete a dormir")  
