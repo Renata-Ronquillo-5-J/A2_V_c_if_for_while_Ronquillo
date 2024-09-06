a8b0c9cf4dedffb23d16b1753e79dfe1b99d7ab6
Renata-Ronquillo <a.22308051281307@cbtis128.edu.mx>
2024-09-06 09:32:49 -0600

    Add files via upload
    Subi dos archivos

---
+++ miscadenas.py
@@ -0,0 +1,27 @@
+'''Renata Ronquillo
+Trabajando con cadenas'''
+
+
+nombre="Renata Ronquillo"
+print(nombre[0])
+print(nombre)
+
+MisNovias=["Agripina", "Aastacia", "Maria"]
+for chicas in MisNovias:
+   print(chicas)
+   
+   
+   print("otro ejemplo")
+   
+b = "Renata" 
+print(b[2:5])
+
+print("a mayusculas")
+a= "renata ronquillo"
+print(a.upper())
+
+print("Concatenar")
+a = "Maria"
+b = "Perez"
+c = a + "_"+ b
+print(c)
---
+++ mivariables.py
@@ -0,0 +1,16 @@
+import math
+edad=17 # 65 se guarda en edad, variable entero
+
+Nombre="Reny Ronquillo" #String Cadena de texto
+print("tu edad es: ", edad )
+print("Your name is:", Nombre)
+print("-- tipo de variables --")
+x = str(7)    # función cadena
+y = int(3.1416)    # entero
+z=float(7)   #decimal
+print(x)
+print(y)
+print(z)
+print("---Funciones Matematicas")
+x = math.sqrt(64) #imprime con decimales porq algunas raizes tienen float. Numero con cadena. Borra el 7 anterior y saca el nuevo 
+print(x)
